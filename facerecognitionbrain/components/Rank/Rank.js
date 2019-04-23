3c546d0fa3abb1ccfd38417da9363624645c7e99
yunglleung1 <yungl.leung1@gmail.com>
2019-04-23 12:27:36 -0400

    Added Rank

---
+++ facerecognitionbrain/components/Rank/Rank.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Rank.css'
+
+const Rank  = () => {
+  return (
+    <div className = 'rank'>
+      <div>Your current rank is...</div>
+      <div className = 'number'>#5</div>
+    </div>
+  )
+}
+
+export default Rank;
